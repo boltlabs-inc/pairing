903b33bce2da999b9295696d89ee9df9fc4b82e3
J. Ayo Akinyele <ayo@boltlabs.io>
2019-09-13 18:37:43 -0400

    Improve Fr/FrRepr to serialize uniformly to array of hex strings instead of uint

--- src/bls12_381/serde_impl.rs
+++ src/bls12_381/serde_impl.rs
@@ -96,7 +96,7 @@ fn deserialize_affine<'de, D: Deserializer<'de>, C: CurveAffine>(d: D) -> Result
 
         #[inline]
         fn visit_str<E>(self, v: &str) -> Result<C, E>
-        where E: ::serde::de::Error,
+            where E: ::serde::de::Error,
         {
             let mut compressed = C::Compressed::empty();
             let _len = C::Compressed::size();
@@ -116,6 +116,28 @@ fn deserialize_affine<'de, D: Deserializer<'de>, C: CurveAffine>(d: D) -> Result
     d.deserialize_str(TupleVisitor{ _ph: PhantomData })
 }
 
+fn transform_u64_to_array_of_u8(x: u64) -> [u8;8] {
+    let b1 : u8 = ((x >> 56) & 0xff) as u8;
+    let b2 : u8 = ((x >> 48) & 0xff) as u8;
+    let b3 : u8 = ((x >> 40) & 0xff) as u8;
+    let b4 : u8 = ((x >> 32) & 0xff) as u8;
+    let b5 : u8 = ((x >> 24) & 0xff) as u8;
+    let b6 : u8 = ((x >> 16) & 0xff) as u8;
+    let b7 : u8 = ((x >> 8) & 0xff) as u8;
+    let b8 : u8 = (x & 0xff) as u8;
+    return [b1, b2, b3, b4, b5, b6, b7, b8]
+}
+
+fn transform_bytes_to_u64(x: &Vec<u8>) -> u64 {
+    let mut u: u64 = 0;
+    let len = x.len() - 1;
+    for i in 0 .. 8 {
+        let t: u64 = (x[len - i] as u64) << (i * 8);
+        u += t;
+    }
+    return u;
+}
+
 impl Serialize for Fr {
     fn serialize<S: Serializer>(&self, s: S) -> Result<S::Ok, S::Error> {
         self.into_repr().serialize(s)
@@ -131,13 +153,54 @@ impl<'de> Deserialize<'de> for Fr {
 
 impl Serialize for FrRepr {
     fn serialize<S: Serializer>(&self, s: S) -> Result<S::Ok, S::Error> {
-        self.0.serialize(s)
+        // self.0.serialize(s)
+        let r = self.0;
+        let len = r.as_ref().len();
+        let mut tup = s.serialize_tuple(len)?;
+        for byte in r.as_ref() {
+            let byte_array = transform_u64_to_array_of_u8(*byte);
+            let hex_str = hex::encode(&byte_array);
+            tup.serialize_element(&hex_str);
+        }
+        tup.end()
     }
 }
 
 impl<'de> Deserialize<'de> for FrRepr {
     fn deserialize<D: Deserializer<'de>>(d: D) -> Result<Self, D::Error> {
-        Ok(FrRepr(<_>::deserialize(d)?))
+        struct FrReprTupleVisitor;
+
+        impl<'de> Visitor<'de> for FrReprTupleVisitor {
+            type Value = FrRepr;
+
+            fn expecting(&self, f: &mut fmt::Formatter) -> fmt::Result {
+                write!(f, "sequence of bytes representing FrRepr element")
+            }
+
+            #[inline]
+            fn visit_seq<A: SeqAccess<'de>>(self, mut seq: A) -> Result<Self::Value, A::Error> {
+                let mut bytes: Vec<u64> = Vec::new();
+                loop {
+                    let tmp = seq.next_element::<String>();
+                    if let Ok(Some(b)) = tmp {
+                        let c = hex::decode(b).to_owned();
+                        bytes.push( transform_bytes_to_u64(&c.unwrap()) );
+                    } else {
+                        break;
+                    }
+                }
+
+                let mut byte_slice: [u64; 4] = [0; 4];
+                if (bytes.len() == 4) {
+                    // let to_err = |_| DeserializeError::custom(ERR_CODE);
+                    byte_slice.copy_from_slice(&bytes[0..4]);
+                }
+                Ok(FrRepr(byte_slice))
+            }
+        }
+
+        //Ok(FrRepr(<_>::deserialize(d)?))
+        d.deserialize_seq(FrReprTupleVisitor {})
     }
 }
 
@@ -209,7 +272,6 @@ mod tests {
         let f: Fr = rng.gen();
         test_roundtrip(&f);
         test_roundtrip(&f.into_repr());
-
     }
 
     #[test]
